dbb147a122072cd7275fa2ee7e90c6f561c789a6
tskim9439 <tskim9439@gmail.com>
2020-08-12 16:54:07 +0000

    conv_s function validated

--- wavenet/.ipynb_checkpoints/data_loader-checkpoint.py
+++ wavenet/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -78,36 +78,4 @@ def save_train_data():
         pickle.dump(sound_data, f)
 
 # %%
-mat_files = glob("/data/datasets/hyundai/20200715/20200703_DNN_ANC_data_acq/PG*.mat")
-mat_files
-#%%
-sound_data = []
-acceleration_data = []
-#with tqdm(mat_files, ncols=100, desc="loading mat data") as _tqdm:
-for i, mat in tqdm(enumerate(mat_files)):
-    #print(mat)
-    mat_data = sio.loadmat(mat, mat_dtype=True)
-    
-    if mat_data['Signal_0'][0,0]['y_values']['values'][0,0].shape[-1] > 8:
-        sig_sound = mat_data['Signal_1']
-        sig_accel = mat_data['Signal_0']
-    else:
-        sig_sound = mat_data['Signal_0']
-        sig_accel = mat_data['Signal_1']
-    
-    value_sound = sig_sound[0,0]['y_values']['values'][0,0][:,:]
-    value_sound = np.array(value_sound)
-    #print("value sound: ", value_sound.shape)
-    sound_data.append(value_sound)
-    value_accel = sig_accel[0,0]['y_values']['values'][0,0][:, :]
-    value_accel = np.array(value_accel)
-    acceleration_data.append(value_accel)
-    #print("acceleration data: ", value_accel.shape)
-
-with open(os.path.join(base_path, "PG_accel_data.pickle"), "wb") as f:
-    pickle.dump(acceleration_data, f)
-
-with open(os.path.join(base_path, "PG_sound_data.pickle"), "wb") as f:
-    pickle.dump(sound_data, f)
-
-# %%
+save_train_data()
--- wavenet/transition_S.py
+++ wavenet/transition_S.py
@@ -12,24 +12,21 @@ S_mat = loadmat(S_mat_path, mat_dtype=True)
 
 S_data = S_mat['S'] # (Ls, K, M)
 
-def Shift(y_buffer, k_idx, value):
-    y_buffer[1:, k_idx] = y_buffer[:-1, k_idx]
-    y_buffer[0, k_idx] = value
-    return y_buffer
+def conv_s(y, s_filter):
+    # defined as a function
+    ## New Conv S => Validated
+    Ls, K, M = s_filter.shape
+    Tn = y.shape[0]
+    y_buffer = torch.zeros((Ls, K))
+    y_p = torch.zeros(y.size())
+    #e = torch.zeros(y.size())
 
-def Conv_S(signal, device='cpu'):
-    #Process S filter to waveform data
-    #the shape of signal should be (batch, time, 8)
-    batch_size = signal.size(0)
-    time_len = signal.size(1)
-    y_pred = torch.zeros((batch_size, time_len, M), device=device)
-    S_filter = torch.Tensor(S_data).to(device) #(Ls, K, M)
-    Y_buffer = torch.zeros((Ls, K), device=device)
-    for batch in range(batch_size):
-        for n in range(time_len):
-            for k in range(K):
-                for m in range(M):
-                    y_pred[batch, n, m] += torch.dot(Y_buffer[:, k], S_filter[:, k, m])
-                    Y_buffer = Shift(Y_buffer, k, signal[batch, n, k])
-        
-    return y_pred
+    for n in range(Tn):
+        for k in range(K):
+            for m in range(M):
+                y_p[n,m] += torch.dot(y_buffer[:, k], s_filter[:, k, m])
+
+        #e[n, :] = d[n, :] - y_p[n, :]
+        y_buffer[1:, :] = y_buffer[:-1, :].clone()
+        y_buffer[0, :] = y[n , :]
+    return y_p
